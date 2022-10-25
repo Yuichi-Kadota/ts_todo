c40f268ecc44a24307ce20cd90d27648018e0349
Yuichi-Kadota <yuichi_kadota@mediado.jp>
2022-10-25 20:10:05 +0900

    fix module

--- todo-app/src/app.module.ts
+++ todo-app/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      // schemaファイルのパスを指定
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // 生成されたschemaを自動でsortされるためのオプションをオンにする
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
